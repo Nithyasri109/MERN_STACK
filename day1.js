638dc59f01dd91fce1b2516998e4d72da96e91ba
Nithyasri109 <kit28.24cs109@gmail.com>
2026-05-20 12:02:38 +0530

    Added Day 1 JavaScript variables and data types task

---
+++ day1.js
@@ -0,0 +1,19 @@
+let studentname="Nithya sri";//string
+let age=20;//number
+let passed=true;//boolean
+let grade;//undefined
+const bignum=898767454342354657n;//bigint
+let address=null;//null
+let hobbies=["reading","painting","dancing"];//array
+let student={
+    name:"Nithya sri",
+    age:20};
+    console.log("Name:",studentname);
+    console.log("Age:",age);
+    console.log("Passed:",passed);
+    console.log("Grade:",grade);
+    console.log("Address:",address);
+    console.log("Hobbies:",hobbies);
+    console.log("Student Object:",student);
+    console.log("BigInt:",bignum);
+
