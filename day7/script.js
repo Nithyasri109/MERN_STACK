f6981561da09e5b56230bef51a709965cfa82e78
Nithyasri109 <kit28.24cs109@gmail.com>
2026-05-26 17:55:04 +0530

    Completed Day 7 - To Do App using Vanilla JS

---
+++ day7/script.js
@@ -0,0 +1,17 @@
+function addTask() {
+
+    let task = document.getElementById("taskInput").value;
+
+    if(task === "") {
+        alert("Enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerText = task;
+
+    document.getElementById("taskList").appendChild(li);
+
+    document.getElementById("taskInput").value = "";
+}
