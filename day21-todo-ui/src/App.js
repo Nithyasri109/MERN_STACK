97bf071f54a2edda1d5211b1ad9ffa5c1f37bb07
Nithyasri109 <kit28.24cs109@gmail.com>
2026-06-10 10:21:02 +0530

    Completed Day 21 - Frontend To-Do UI

---
+++ day21-todo-ui/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() === "") return;
+
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>To-Do App</h1>
+
+      <input
+        type="text"
+        placeholder="Enter Task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={addTask}>
+        Add
+      </button>
+
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
